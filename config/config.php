982fe6a79cf1aecd6282395af73f8275e88613c4
josephsb <jguku2002@gmail.com>
2023-04-26 15:48:41 -0500

    inicio del proyecto

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    define('URL', 'http://localhost/MagicCakes/');
+    //-------------DB--------------
+    define('HOST', 'localhost');
+    define('DB', 'minispotify');
+    define('USER', 'root');
+    define('PASSWORD', "");
+    define('CHARSET', 'utf8mb4');
+
+?>
---
+++ views/Home/index.php
@@ -0,0 +1,13 @@
+<?php
+    $BASE_URL = constant('URL')."public";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Magic Cakes</title>
+</head>
+<body>
+
+</body>
+</html>
