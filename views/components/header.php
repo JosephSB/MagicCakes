5ba285729b0929a7d358848c667af5671dfadf58
keyla188 <kcabreraalcala@gmail.com>
2023-05-01 21:58:45 -0500

    añadi header - keyla

---
+++ views/components/header.php
@@ -0,0 +1,25 @@
+<header class="header">
+    <nav>
+        <input type="checkbox" id="check" />
+        <label for="check" class="checkbtn">
+            <i class="fa-solid fa-bars fa-lg" style="color: #000000;"></i>
+        </label>
+        <a href="#">
+            <img src="<?php echo  $BASE_URL; ?>/assets/magiccakes.png" height="80px" />
+        </a>
+
+        <ul>
+            <li><a class="navegacion" href="#">Promociones</a></li>
+            <li><a class="navegacion" href="#">Nuestra Carta</a></li>
+            <li><a class="navegacion" href="#">Nuestras tiendas</a></li>
+        </ul>
+        <div class="icon-right">
+            <a href="#">
+                <i class="fa-regular fa-heart fa-lg" style="color: #000000;"></i>
+            </a>
+            <a href="#">
+                <i class="fa-sharp fa-solid fa-cart-shopping fa-lg" style="color: #000000;"></i>
+            </a>
+        </div>
+    </nav>
+</header>
